33e78b51a4928f947bafa21182bab63b947c8c41
Kurt Friars <kurt@plankdesign.com>
2023-02-09 16:27:33 -0500

    Reduce coognitive complexity of extractor

--- src/RelationExtractor.php
+++ src/RelationExtractor.php
@@ -20,39 +20,45 @@ class RelationExtractor
         $class = new ReflectionClass($model);
 
         return (new Collection($class->getMethods()))
-            ->filter(function (ReflectionMethod $method) {
-                if (! $method->isPublic() || $method->isStatic()) {
-                    return false;
-                }
-
-                if (count($method->getParameters())) {
-                    return false;
-                }
-
-                if ($method->class === Model::class) {
-                    return false;
-                }
-
-                if ($returnType = $method->getReturnType()) {
-                    return is_a($returnType->getName(), Relation::class, true);
-                }
-
-                return true;
-            })
-            ->map(function (ReflectionMethod $method) use ($model) {
-                try {
-                    $relation = @$model->{$method->getName()}();
-                } catch (\Exception $e) {
-                    return null;
-                }
-
-                if ($relation instanceof Relation) {
-                    return new ImplementedRelation($method, $relation);
-                }
-
-                return null;
-            })
+            ->filter(fn (ReflectionMethod $method) => $this->hasRelationSignature($method))
+            ->map(fn (ReflectionMethod $method) => $this->toImplementedRelation($model, $method))
             ->filter()
             ->values();
+    }
+
+    protected function hasRelationSignature(ReflectionMethod $method): bool
+    {
+        if (! $method->isPublic() || $method->isStatic()) {
+            return false;
+        }
+
+        if (count($method->getParameters())) {
+            return false;
+        }
+
+        if ($method->class === Model::class) {
+            return false;
+        }
+
+        if ($returnType = $method->getReturnType()) {
+            return is_a($returnType->getName(), Relation::class, true);
+        }
+
+        return true;
+    }
+
+    protected function toImplementedRelation(Model $model, ReflectionMethod $method): ?ImplementedRelation
+    {
+        try {
+            $relation = @$model->{$method->getName()}();
+        } catch (\Exception $e) {
+            return null;
+        }
+
+        if ($relation instanceof Relation) {
+            return new ImplementedRelation($method, $relation);
+        }
+
+        return null;
     }
 }
